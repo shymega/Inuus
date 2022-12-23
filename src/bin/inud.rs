e6a5791c75f057254c54bad72cfd28ce438752ab
Dom Rodriguez <shymega@shymega.org.uk>
2022-12-23 17:02:10 +0000

    Run rustfmt on codebase
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/bin/inud.rs
+++ src/bin/inud.rs
@@ -19,8 +19,8 @@
 
 use std::path::PathBuf;
 
-use clap::{Arg, ArgMatches, Command, ArgAction};
-use config::{Config, File, Environment, ConfigError};
+use clap::{Arg, ArgAction, ArgMatches, Command};
+use config::{Config, ConfigError, Environment, File};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -28,16 +28,17 @@ fn try_load_conf(path: &PathBuf) -> Result<Config, ConfigError> {
     match Config::builder()
         .add_source(File::from(path.as_path().clone()))
         .add_source(Environment::with_prefix("INUD"))
-        .build() {
-            Ok(cfg) => Ok(cfg),
-            Err(e) => Err(e),
-        }
+        .build()
+    {
+        Ok(cfg) => Ok(cfg),
+        Err(e) => Err(e),
+    }
 }
 
 fn get_cfg_path_default() -> Option<PathBuf> {
-    let def_path =  dirs::config_dir()
-            .unwrap()
-            .join(PathBuf::from("inuus/config.toml"));
+    let def_path = dirs::config_dir()
+        .unwrap()
+        .join(PathBuf::from("inuus/config.toml"));
 
     if def_path.exists() {
         Some(def_path)
@@ -51,15 +52,18 @@ fn get_args() -> ArgMatches {
         .version(VERSION)
         .author("Dom Rodriguez")
         .subcommand_required(true)
-        .arg(Arg::new("verbosity")
-            .short('v')
-            .action(ArgAction::Count)
-            .help("Increment to get verbose logs."))
-        .arg(Arg::new("config")
-            .short('c')
-            .help("Optional path to TOML configuration."))
-        .subcommand(Command::new("spawn")
-            .about("Starts the daemon."))
+        .arg(
+            Arg::new("verbosity")
+                .short('v')
+                .action(ArgAction::Count)
+                .help("Increment to get verbose logs."),
+        )
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .help("Optional path to TOML configuration."),
+        )
+        .subcommand(Command::new("spawn").about("Starts the daemon."))
         .get_matches()
 }
 
@@ -69,7 +73,9 @@ async fn main() -> Result<(), ()> {
 
     let arg_cfg_path = match args.get_one::<PathBuf>("config") {
         Some(p) => PathBuf::from(p),
-        None => get_cfg_path_default().expect("Unable to get a default conf path."),
+        None => {
+            get_cfg_path_default().expect("Unable to get a default conf path.")
+        }
     };
 
     let _cfg = try_load_conf(&arg_cfg_path);
