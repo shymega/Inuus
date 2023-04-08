90fab18edb7357055df13d4eb8c6e9a7da5b3874
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-08 22:43:48 +0100

    Explain reason for get_secret_by_path using &str
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/secrets_providers/mod.rs
+++ src/secrets_providers/mod.rs
@@ -8,7 +8,7 @@ pub trait SecretsProviderTrait {
     fn get_secret_by_uuid(uuid: &str) -> String;
     /// `get_secret_by_path(&str)` takes a slash-delimited `&str` to a `secret`, and returns a
     /// `String` of the secret's content.
-    fn get_secret_by_path(path: &str) -> String; // we use a `&str` for the path, because `pass` isn't a Rust program.
+    fn get_secret_by_path(path: &str) -> String; // we use a `&str` for the path, because `pass` doesn't use `Path` or `PathBuf`.
 }
 
 #[cfg(feature = "bitwarden")]
