cea6434d47a33ef5caf949f22fd0a34e90b8075b
Dom Rodriguez <shymega@shymega.org.uk>
2023-06-12 18:08:05 +0100

    Rename type alias for `Mutex<Keys>`

--- src/agent.rs
+++ src/agent.rs
@@ -1,17 +1,17 @@
 //! This `module` defines the Inuus agent as a struct and implementation, for usage by the user,
 //! and also by the JSON-RPC client.
 
-use crate::models::keys::Keys;
+use crate::models::keys::Keys as KeysModel;
 use std::sync::Mutex;
 
-/// This type alias defines `KeysMutex` to a `Mutex` of the `Keys` struct defined in
-/// `crate::models::keys::Keys`.
-pub type KeysMutex = Mutex<Keys>;
+/// This type alias defines `Keys` to a `Mutex` of the `Keys` (aliased as `KeysModel`) struct
+/// defined in `crate::models::keys::Keys`.
+type Keys = Mutex<KeysModel>;
 
 /// `InuusAgent` holds one (unused, lint disabled) field for the shared `Mutex<Keys>` which
 /// contains a `Vec<KeyContainer>` of keys, both private and public.
 #[derive(Debug)]
 #[allow(dead_code)]
 pub struct InuusAgent {
-    keys: KeysMutex,
+    keys: Keys,
 }
