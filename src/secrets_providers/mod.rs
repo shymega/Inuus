427d19fbff51db6dd1da9c10fdb068107b0fcdc6
Dom Rodriguez <shymega@shymega.org.uk>
2023-08-22 21:04:48 +0100

    feat(secrets): Implement stub methods for `SecretsProviderTrait`

--- src/secrets_providers/mod.rs
+++ src/secrets_providers/mod.rs
@@ -5,10 +5,15 @@ pub(crate) trait SecretsProviderTrait {
     /// `get_secret_by_uuid(&str)` takes a UUID of a 'secret', and returns a `String` of the
     /// secret's content.
     /// TODO: Change to shared representation of a SSH key.
-    fn get_secret_by_uuid(uuid: &str) -> String;
+    fn get_secret_by_uuid(_uuid: &str) -> String {
+        String::new()
+    }
     /// `get_secret_by_path(&str)` takes a slash-delimited `&str` to a `secret`, and returns a
     /// `String` of the secret's content.
-    fn get_secret_by_path(path: &str) -> String; // we use a `&str` for the path, because `pass` doesn't use `Path` or `PathBuf`.
+    /// We use a `&str` for the path, because `pass` doesn't use `Path` or `PathBuf`.
+    fn get_secret_by_path(_path: &str) -> String {
+        String::new()
+    }
 }
 
 #[cfg(feature = "bitwarden")]
